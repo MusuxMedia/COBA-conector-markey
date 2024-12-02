7c557dc7e339b4e1fc6ab17ff79acf7dd45d4dd4
MusuxMedia <soporte@musuxmedia.com>
2024-12-02 15:45:01 -0300

    Agrego el docker y cambio los customertypes

--- app/conector/config.py
+++ app/conector/config.py
@@ -5,6 +5,6 @@ class Settings(BaseSettings):
     MARKEY_URL: str = "https://coba.markey.com.ar/APIMarkeyV2/obtener"
     MARKEY_TOKEN: str = '9b94d8c8-e6b6-447a-91c5-62e377ba17b3'
     MARKEY_API_KEY: str = "b473552d-cbe8-42c2-8de9-c8aad68bf6e0"
-    CUSTOMERTYPE: str = "829"
-    NO_CUSTOMERTYPE: str = "799"
+    CUSTOMERTYPE: str = "919"
+    NO_CUSTOMERTYPE: str = "917"
     OPERATION: str = "obtenerTurnosPaciente"
